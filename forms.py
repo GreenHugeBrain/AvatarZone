8329db0dd1ac92f5fd906118255ab932a0a578e4
GreenHugeBrain <142096498+GreenHugeBrain@users.noreply.github.com>
2024-08-14 17:53:07 +0400

    Add files via upload

--- forms.py
+++ forms.py
@@ -3,11 +3,11 @@ from wtforms import StringField, PasswordField, SubmitField, SelectField
 from wtforms.validators import DataRequired, InputRequired
 
 class RegistrationForm(FlaskForm):
-    username = StringField('სახელი', validators=[DataRequired()])
-    email = StringField('მეილი', validators=[DataRequired()])
-    password = PasswordField('პაროლი', validators=[DataRequired()])
+    username = StringField('მომხმარებლის სახელი', validators=[DataRequired()])
+    email = StringField('ელ. ფოსტა:', validators=[DataRequired()])
+    password = PasswordField('პაროლი:', validators=[DataRequired()])
     groupID = SelectField('ვადა', choices=[(5, "5 დღე"), (14, "2 კვირა"), (30, "1 თვე")], validators=[InputRequired()])
-    submit = SubmitField('დარეგისტრირდი')
+    submit = SubmitField('რეგისტრაცია')
 
 class LoginForm(FlaskForm):
     username = StringField('სახელი', validators=[DataRequired()])
